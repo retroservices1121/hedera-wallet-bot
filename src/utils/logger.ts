1d7221864356bff9fb97df332c2c457320f016bb
retroservices1121 <76626942+retroservices1121@users.noreply.github.com>
2025-12-03 18:47:53 -0500

    Create logger.ts

---
+++ src/utils/logger.ts
@@ -0,0 +1,23 @@
+import pino from "pino";
+import { config } from "../config";
+
+export const logger = pino({
+  level: config.logLevel,
+  transport:
+    config.environment === "development"
+      ? {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "SYS:standard",
+            ignore: "pid,hostname",
+          },
+        }
+      : undefined,
+  formatters: {
+    level: (label) => {
+      return { level: label };
+    },
+  },
+  timestamp: pino.stdTimeFunctions.isoTime,
+});
